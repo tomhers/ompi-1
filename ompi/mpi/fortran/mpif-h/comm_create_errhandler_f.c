0f2029a21cfafb311642b35279aa10ade42517df
Howard Pritchard <hppritcha@gmail.com>
2018-12-18 09:30:18 -0700

    temporary fix for fortran error handlers
    Signed-off-by: Howard Pritchard <hppritcha@gmail.com>
    (cherry picked from commit 04314a099d819091a4b2e15fcb22dbddfc5c809e)

--- ompi/mpi/fortran/mpif-h/comm_create_errhandler_f.c
+++ ompi/mpi/fortran/mpif-h/comm_create_errhandler_f.c
@@ -74,10 +74,10 @@ void ompi_comm_create_errhandler_f(ompi_errhandler_fortran_handler_fn_t *functio
 				  MPI_Fint *errhandler, MPI_Fint *ierr)
 {
     int c_ierr;
-    MPI_Errhandler c_errhandler =
-        ompi_errhandler_create(OMPI_ERRHANDLER_TYPE_COMM,
+    MPI_Errhandler c_errhandler;
+    c_ierr = ompi_errhandler_create(OMPI_ERRHANDLER_TYPE_COMM,
                                (ompi_errhandler_generic_handler_fn_t*) function,
-                               OMPI_ERRHANDLER_LANG_FORTRAN);
+                               OMPI_ERRHANDLER_LANG_FORTRAN, &c_errhandler);
     if (MPI_ERRHANDLER_NULL != c_errhandler) {
         *errhandler = PMPI_Errhandler_c2f(c_errhandler);
         c_ierr = MPI_SUCCESS;
